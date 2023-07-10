a498d5bd5be5627a40b8ed99fa8dfa58abc714cc
slitasov <43509889+slitasov@users.noreply.github.com>
2023-07-11 00:48:02 +0300

    Add files via upload

---
+++ Dependencies.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# cadCAD standard dependencies
+
+# cadCAD configuration modules
+from cadCAD.configuration.utils import config_sim
+from cadCAD.configuration import Experiment
+
+# cadCAD simulation engine modules
+from cadCAD.engine import ExecutionMode, ExecutionContext
+from cadCAD.engine import Executor
+
+# cadCAD global simulation configuration list
+from cadCAD import configs
+
+# Included with cadCAD
+import pandas as pd
+
+
+# In[2]:
+
+
+import random
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import mplcyberpunk
+
+
+# In[ ]:
+
+
+
+
